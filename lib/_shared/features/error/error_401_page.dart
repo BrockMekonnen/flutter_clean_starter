8c6190a216cac40de7e1476e335e3907ec81f5a6
Biruk Mekonnen <birukmekonnent@gmail.com>
2025-05-27 21:25:41 +0300

    refactor: remove unused import of app_router in error pages

--- lib/_shared/features/error/error_401_page.dart
+++ lib/_shared/features/error/error_401_page.dart
@@ -1,4 +1,3 @@
-import 'package:clean_starter/_core/app_router.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
